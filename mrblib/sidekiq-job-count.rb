3a5355b851aaf56bafe678d8004385e71070c1a8
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-10-28 18:33:04 +0900

    calculate all of queue length

--- mrblib/sidekiq-job-count.rb
+++ mrblib/sidekiq-job-count.rb
@@ -4,11 +4,16 @@ def __main__(argv)
   else
     r = Redis.new argv[1], argv[2].to_i
     namespace = argv[3]
+
     key = ""
     key += "#{namespace}:" if namespace
     key += 'queues'
-    queues = r.scard key
+
+    queues = r.smembers key
+    jobs = queues.map{|queue| "#{key[0...-1]}:#{queue}" }.map{|k| r.llen k }.inject(0){|s, v| s + v}
+
     r.close
-    puts "#{key}\t#{queues}\t#{Time.now.to_i}"
+
+    puts "#{key}\t#{jobs}\t#{Time.now.to_i}"
   end
 end
