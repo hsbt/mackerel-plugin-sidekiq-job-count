50dabbc641bcb2efcf7f20b5206cb6c95bd1198e
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2015-11-27 16:48:35 +0900

    added more stats

--- mrblib/mackerel-plugin-sidekiq-job-count.rb
+++ mrblib/mackerel-plugin-sidekiq-job-count.rb
@@ -2,17 +2,23 @@ def __main__(argv)
   if argv[1] == "version"
     puts "v#{SidekiqJobCount::VERSION}"
   else
+    res = Hash.new
     r = Redis.new argv[1], argv[2].to_i
     namespace = argv[3]
 
     prefix = ""
     prefix += "#{namespace}:" if namespace
 
-    queues = r.smembers prefix + "queues"
-    enqueued = queues.map{|queue| "#{prefix}queue:#{queue}" }.map{|k| r.llen k }.inject(0){|s, v| s + v}
+    queues = r.smembers "#{prefix}queues"
+    res[:enqueued] = queues.map{|queue| "#{prefix}queue:#{queue}" }.map{|k| r.llen k }.inject(0){|s, v| s + v}
+    res[:retries] = r.zcard "#{prefix}retry"
+    res[:scheduled] = r.zcard "#{prefix}schedule"
+    res[:dead] = r.zcard "#{prefix}dead"
 
     r.close
 
-    puts "sidekiq_job_count.enqueued\t#{enqueued}\t#{Time.now.to_i}"
+    res.each do |k,v|
+      puts "sidekiq_job_count.#{k}\t#{v}\t#{Time.now.to_i}"
+    end
   end
 end
